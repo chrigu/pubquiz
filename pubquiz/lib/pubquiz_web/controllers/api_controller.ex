e81b9d8ce4e75ba85517245816a1abfcba8c7cf4
Christian Cueni <christian.cueni@iterativ.ch>
2021-02-07 20:10:35 +0100

    Add start game

--- pubquiz/lib/pubquiz_web/controllers/api_controller.ex
+++ pubquiz/lib/pubquiz_web/controllers/api_controller.ex
@@ -1,12 +1,37 @@
 defmodule PubquizWeb.ApiController do
   use PubquizWeb, :controller
 
+  alias PubquizGame.{GameServer, GameSupervisor}
+
   def start(conn, params) do
-      user = %{name: "Hans"}
-      IO.inspect(conn)
-      IO.inspect(params)
-      conn
-      |> put_status(:created)
-      |> render("index.json", user: user)
+    game_name = Pubquiz.HaikuName.generate()
+
+    player = %{name: "Hans"}
+
+    case GameSupervisor.start_game(game_name) do
+      {:ok, _game_pid} ->
+        conn
+#        |> put_session(:player, player)
+        |> redirect(to: "/" <> game_name)
+
+      {:error, _error} ->
+        conn
+        |> put_flash(:error, "Unable to start game!")
+        |> redirect(to: "/game/fail")
+    end
+
   end
 end
+
+#    game_name = BingoHall.HaikuName.generate()
+#    size = String.to_integer(size)
+#
+#    case GameSupervisor.start_game(game_name, size) do
+#      {:ok, _game_pid} ->
+#        redirect(conn, to: game_path(conn, :show, game_name))
+#
+#      {:error, _error} ->
+#        conn
+#        |> put_flash(:error, "Unable to start game!")
+#        |> redirect(to: game_path(conn, :new))
+#    end
